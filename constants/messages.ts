b9734b25cc1f8eb961dc549c16c435daf82867f1
mass-illusion <sabrina.dn.tran@gmail.com>
2026-01-09 13:46:46 -0800

    99999

--- constants/messages.ts
+++ constants/messages.ts
@@ -1,3 +1,4 @@
 export const CUSTOM_MESSAGES = {
   UNIVERSAL_INTRO: "Hi! I'm excited to connect with you. What brings you to this event?",
+  LOCALS_MAP_INTRO: "Hey! I saw we have some shared interests. Would love to connect 🙂",
 };
